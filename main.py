8deb80c96dfd2f98560cf36f02fc1915b0bb34e5
MageniC <168565191+MageniC0@users.noreply.github.com>
2024-06-28 22:48:10 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+print("[core]version:alpha/1.2.0")
+print("[core]dawn script...")
+
+print("[dvclv]live dustormn.")
+
+print("[flu]live flu.")
+
+print("[vale]live vale.")
+
+print(".ytilaer evil[303]")
+print("[core]-1")
+print("[laer]live laer")
