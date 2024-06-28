12dee3f4d4498562433156912aa8095619170bf3
MageniC <168565191+MageniC0@users.noreply.github.com>
2024-06-29 01:17:34 +0800

    .

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 print("[core]version:alpha/1.2.0")
 print("[core]dawn script...")
-import flu
-import evil
-import vally
-
-if __name__ == "__main__":
-    filename = input("请输入JSON文件名: ")
-    flu.manage_json(filename)
-
+import laer.flu as flu
 print("[dvclv]live dustormn.")
 
 print("[flu]live flu.")
 
+if __name__ == "__main__":
+    filename = input("name:")
+    flu.manage_json(filename)
+
 print("[vale]live vale.")
 
-print(".ytilaer evil[303]")
+print("[303].ytilaer evil")
 print("[core]-1")
 print("[laer]live laer")
