485e554b45cb70cdc164da3a277d95ed8c5eea03
MageniC <168565191+MageniC0@users.noreply.github.com>
2024-06-29 01:38:23 +0800

    .

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-print("[core]version:alpha/1.2.0")
-print("[core]dawn script...")
+print("[core]version:1.2.0/alpha.")
 import laer.flu as flu
-print("[dvclv]live dustormn.")
+import laer.valley as valley
+import laer.laer as laer
+print("[core]script dawn.")
 
-print("[flu]live flu.")
+
+def main():
+    
+    def boat():
+        su = input("[core]exit?").lower()
+        if su == "1":
+            return()
+        if su == "0":
+            print("[core]done.")
+            exit()
+    
+    print("[dvclv]live dustormn.")
+    print("1_flu")
+    print("2_vale")
+    print("3_laer")
+    
+    inp = input("_")
+    
+    if inp == "1":
+        print("[flu]live flu.")
+        filename = input("name:")
+        flu.manage_json(filename)
+        boat()
+    
+    elif inp == "2":
+        print("[vale]live vale.")
+        #
+        
+        
+        #
+        boat()
+    
+    elif inp == "3":
+        print("[303].ytilaer evil")
+        #
+        
+        
+        #
+        boat()
+    
+    else:
+        print("???")
 
 if __name__ == "__main__":
-    filename = input("name:")
-    flu.manage_json(filename)
-
-print("[vale]live vale.")
-
-print("[303].ytilaer evil")
-print("[core]-1")
-print("[laer]live laer")
+    main()
