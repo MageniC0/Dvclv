3967521dcd6ccec6307b2b6d4fbc3c093ffd9f41
MageniC <168565191+MageniC0@users.noreply.github.com>
2024-07-07 13:33:17 +0800

    .

--- g1.py
+++ g1.py
@@ -3,9 +3,10 @@ def f():
     h1 = os.path.dirname(os.path.abspath(__file__))
     h2 = 0
     h3 = ''
+    h7 = h1.count()
     for h4, h5, h6 in os.walk(h1):
         h5.sort()
-        h2 = h4.count(os.sep) - h1.count(os.sep)
+        h2 = h4.count(os.sep) - h7
         h3 = '_' * h2
         for h8 in h5:
             print(f"{h3}~{h8}")
