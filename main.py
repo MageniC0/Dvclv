6ed15d3c85ef16b52403d1078382968b594b4201
MageniC <168565191+MageniC0@users.noreply.github.com>
2024-07-07 13:27:54 +0800

    .

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 print("[星光]1.6.0")
 print("")
-print("输入0返回")
-print("输入数字继续")
 print("[主页面]")
-print("1_ 检查文件")
-print("2_ 生成图形")
-print("3_ 设计资源包")
-print("4_ 设计地图")
-print("5_ 参考文档")
-
 i = input("")
 
 import g1
@@ -32,6 +24,6 @@ if i == "4":
     g4.f()
 
 if i == "5":
-    file = open("laer.txt","r")
-    print(file)
-    file.close
+    laer = open("laer.txt","r")
+    print(laer.read())
+    laer.close
