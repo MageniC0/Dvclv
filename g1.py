3f31d061de5604f6a7bc1619b30b759bfb34b867
MageniC <168565191+MageniC0@users.noreply.github.com>
2024-07-07 13:04:27 +0800

    .

--- g1.py
+++ g1.py
@@ -5,9 +5,9 @@ def f():
     h3 = ''
     for h4, h5, h6 in os.walk(h1):
         h5.sort()
-        h7 = h4.count(os.sep) - h1.count(os.sep)
-        h3 = '_' * h7
+        h2 = h4.count(os.sep) - h1.count(os.sep)
+        h3 = '_' * h2
         for h8 in h5:
             print(f"{h3}~{h8}")
         for h0 in h6:
-            print(f"{h3}_ {h0}")
+            print(f"{h3}_{h0}")
