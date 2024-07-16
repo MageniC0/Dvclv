5473168507cfb37d75c89580b58cd152d9fd7a0a
MageniC <168565191+MageniC0@users.noreply.github.com>
2024-07-16 23:20:33 +0800

    。

--- laer.py
+++ laer.py
@@ -1,36 +1,64 @@
-﻿import os,pickle,json
+import os,pickle,json,time
 from PIL import Image,ImageDraw
-#本程序[starlit星光]由Dustormn风尘结社开发，由后勤部负责人MageniC（磁砖）主持开发工作，受到版权法保护。
-#禁止私自转载或商业性质使用。
-#此版本为没有文本色彩样式的副本
+def s0():
+    print("\033[92m"+"_"*68)
 def s1():
-    print("_"*68)
-def s2(n):
-    print("|   "*n)
-def s3(s,n):
-    print('|   '*n+s)
-def s4(s,n):
-    a = input('|   '*n+s)
+    print("\033[92m|   ")
+def s2():
+    print("\033[92m|   |   ")
+def s3(s):
+    print(f"\033[92m|   \033[0m{s}")
+def s4(s):
+    print(f"\033[92m|   |   \033[0m{s}")
+def s5(s,m):
+    print(f"\033[92m|   \033[0m{s}\033[94m{m}")
+def s6(s,m):
+    print(f"\033[92m|   |   \033[0m{s}\033[94m{m}")
+def s7(s):
+    a = input(f"\033[92m|   \033[0m{s}\033[94m_")
+    if len(a) == 0:
+        return "0"
+    return a
+def s8(s):
+    a = input(f"\033[92m|   |   \033[0m{s}\033[94m_")
     if len(a) == 0:
         return "0"
     return a
 def h0(s):
     while True:
-        s2(2)
-        u = s4(f"[{s}]选择方块_",2)
+        s2()
+        u = s8(f"[{s}]选择方块")
         try:
             u = int(u)
             if 0 <= u <= 255:
                 return u
             else:
-                s3(f"[提示]输入数字于0～255",3)
+                s4(f"[提示]输入数字于0～255")
         except ValueError:
-            s3(f"[提示]输入数字于0～255",3)
+            s4(f"[提示]输入数字于0～255")
+def h1(s):
+    s4(s)
+    x = int(s8("x"))
+    y = int(s8("y"))
+    z = int(s8("z"))
+    return x,y,z
+def h2(u,v):
+    if u == 0:
+        s3("[地质]正在拆除")
+        return 0
+    else:
+        s5(f"[地质]持有方块",v)
+        return 1
 def g0():
-    u,m,n,j = s4("[材质]方块名_",3),s4("[材质]方块主色_",3),s4("[材质]方块副色_",3),0
+    u = s8("[材质]方块名")
+    m = s8("[材质]方块主色")
+    n = s8("[材质]方块副色")
+    j = 0
     c0,c1,c2,c3,c4,c5 = int(m[0:2],16),int(m[2:4],16),int(m[4:6],16),int(n[0:2],16),int(n[2:4],16),int(n[4:6],16)
     c6,c7,c8,c9,c10,c11 = int(c0*0.8),int(c1*0.8),int(c2*0.8),int(c3*0.8),int(c4*0.8),int(c5*0.8)
-    s3(f"[材质]构造方块_{u}",3)
+    s0()
+    print()
+    time.sleep(0.2)
     i = Image.new('RGB',(208,208), color=(0,0,0))
     l = ImageDraw.Draw(i)
     a = "0000011100000001111111110022111111111223322211122233333332223333333333343333333333334333333333333433333333333343333333333334333333333333433333300333343333300000034300000"
@@ -48,19 +76,22 @@ def g0():
                 l.rectangle(pos,fill=(c9,c10,c11))
             j+=1
     i.show()
+    s0()
+    s6(f"[材质]构造方块",u)
     return [c0,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11],u
 def g1():
-    u1 = "r/ch/" + s4("[材质]指定资源包_",2) + "/"
+    u1 = s7("[材质]指定资源包")
+    u1 = f"r/ch/{u1}/"
     if os.path.exists(u1):
         with open(u1+'a','rb') as f:u2 = pickle.load(f)
         with open(u1+'b','rb') as f:u3 = pickle.load(f)
     else:
         os.makedirs(u1)
         u2 = [[0 for _ in range(12)] for _ in range(256)]
-        u3 = ["dust" for _ in range(256)]
+        u3 = ["dvst" for _ in range(256)]
         with open(u1+'a','wb') as f:pickle.dump(u2,f)
         with open(u1+'b','wb') as f:pickle.dump(u3,f)
-        s3(f"[材质]创建_{u1}",2)
+        s5(f"[材质]创建",u1)
     while True:
         u4 = h0("材质")
         if u4 == 0:
@@ -71,48 +102,37 @@ def g1():
             u3[u4] = u6
             with open(u1+'a','wb') as f:pickle.dump(u2,f)
             with open(u1+'b','wb') as f:pickle.dump(u3,f)
-def h1(s):
-    s3(s,3)
-    x = int(s4("x_",3))
-    y = int(s4("y_",3))
-    z = int(s4("z_",3))
-    return x,y,z
-def g2(a,b,c,d):
+def g2(a,b,c):
     with open(a+'a',"wb") as f:pickle.dump(b,f)
     with open(a+'b',"wb") as f:pickle.dump(c,f)
-    s3(d,3)
-def h2(u,v):
-    if u == 0:
-        s3("[地质]正在拆除",2)
-        return 0
-    else:
-        s3(f"[地质]持有方块_{v}",2)
-        return 1
 def g3(l):
-    c = "r/ch/" + s4("[制图]指定资源包_",2) + "/"
+    c = s7("[制图]指定资源包")
+    c = f"r/ch/{c}/"
     if os.path.exists(c):
         with open(c+'a','rb') as f:a = pickle.load(f)
         with open(c+'b','rb') as f:b = pickle.load(f)
     else:
-        s3("[雨声]未找到资源包",2)
+        s5("[雨声]未找到资源包",c)
         return
-    t = "r/tr/" + s4("[制图]指定地图册_",2) + "/"
+    t = s7("[制图]指定地图册")
+    t = f"r/tr/{t}/"
     if os.path.exists(t):
         with open(t+'a','rb') as f:ta = pickle.load(f)
         with open(t+'b','rb') as f:tb = pickle.load(f)
     else:
         os.makedirs(t)
-        s3(f"[地质]创建{t}",2)
+        s5("[地质]创建",t)
         ta = [[[[0 for _ in range(12)] for _ in range(16)] for _ in range(16)] for _ in range(16)]
         tb = [[[0 for _ in range(16)] for _ in range(16)] for _ in range(16)]
         with open(t+'a','wb') as f:pickle.dump(ta,f)
         with open(t+'b','wb') as f:pickle.dump(tb,f)
     if len(l) == 1:
-        s3("",2)
-        s3("2.1 放置方块",2)
-        s3("2.2 放置方块组",2)
-        s3("2.3 选择笔刷",2)
-        l = "2." + s4(f"[地质]选择支线 2.",2)
+        s3("2.1 放置方块")
+        s3("2.2 放置方块组")
+        s3("2.3 选择笔刷")
+        s1()
+        l = s7(f"[地质]选择支线 2.")
+        l = f"2.{l}"
     if l == "2.0":
         return
     d = 1
@@ -125,7 +145,8 @@ def g3(l):
             x,y,z = h1("[地质]选择坐标")
             ta[x][y][z] = p
             tb[x][y][z] = d
-            g2(t,ta,tb,f"[地质]在({x},{y},{z})放置方块_{q}")
+            g2(t,ta,tb)
+            s6(f"[地质]在({x},{y},{z})放置方块",q)
         elif l == "2.2":
             x1,y1,z1 = h1("[地质]选择起点坐标")
             x2,y2,z2 = h1("[地质]选择终点坐标")
@@ -134,25 +155,28 @@ def g3(l):
                     for z in range(z1,z2+1):
                         ta[x][y][z] = p
                         tb[x][y][z] = d
-            g2(t,ta,tb,f"[地质]从({x1},{y1},{z1})到({x2},{y2},{z2})放置方块_{q}")
+            g2(t,ta,tb)
+            s6(f"[地质]从({x1},{y1},{z1})到({x2},{y2},{z2})放置方块",q)
         elif l == "2.3":
             w = h0("材质")
             p = a[w]
             q = b[w]
             d = h2(w,q)
-            s3("",2)
-        s2(2)
-        l = "2." + s4("2._",2)
+        s1()
+        l = s7("2.")
+        l = f"2.{l}"
 def g4():
-    t = "r/tr/" + s4("[制图]指定地图册_",2) + "/"
+    t = s7("[制图]指定地图册")
+    t = f"r/tr/{t}/"
     if os.path.exists(t):
         with open(t+'a','rb') as f:u2 = pickle.load(f)
         with open(t+'b','rb') as f:u3 = pickle.load(f)
     else:
-        s3(f"[雨声]未找到_{t}",2)
+        s5(f"[雨声]未找到",t)
         return
-    u1 = s4("[制图]指定文件名_",2)+".png"
-    s3("[制图]传送数据...",2)
+    u1 = s7("[制图]指定文件名")
+    u1 = f"{u1}.png"
+    s3("[制图]传送数据...")
     u4 = [[[0 for _ in range(18)] for _ in range(18)] for _ in range(18)]
     for x in range(16):
         for y in range(16):
@@ -228,12 +252,16 @@ def g4():
     u0=Image.new('RGB',(193,193), color=(255, 255, 255))   
     u0.putdata([l for w in p for l in w])
     u0.save(u1)
-    print(f"\n[制图]生成图像[{u1}]")
+    s0()
+    s3(f"\n[制图]生成图像\033[94m{u1}")
+    print("\n")
+    time.sleep(0.2)
     u0.show()
+    s0()
 def f0():
     l=[]
-    s3("[检查]传送数据...",1)
-    s1()
+    s3("[检查]传送数据...")
+    s0()
     print("[查看文件目录]")
     u=os.path.abspath('.')
     for v,_,x in os.walk(u):
@@ -242,30 +270,33 @@ def f0():
         l.append(h+'_'*(34 - len(h))+"\n")
         m='|   '*(y+1)
         l.extend(f'{m}{n}\n' for n in x)
-    print("".join(l))
-    s1()
+    print("\033[90m"+"".join(l))
+    s0()
 def f1():
-    h = s4(f"[检查]指定资源包_",1)
+    h = s7(f"[检查]指定资源包")
     try:
         with open(f"r/ch/{h}/b","rb") as f:c=pickle.load(f)
     except Exception:
-        s3(f"[雨声]未找到资源包_{h}",1)
+        s5(f"[雨声]未找到资源包",h)
+        return
     p = []
     for i in range(64):
         for j in range(4):
             k=i+j*64
             p.append(f"[{k}]_{c[k]}".ljust(20))
         p.append("\n")
-    s1()
-    print(f"[检查]查看资源包_{h}")
-    print(''.join(p))
-    s1()
+    s0()
+    print(f"[检查]查看资源包",h)
+    print("\033[90m"+"".join(p))
+    s0()
 def f2():
-    t = "r/tr/" + s4("[检查]指定地图册_",1) + "/"
+    t = s7("[检查]指定地图册")
+    t = f"r/tr/{t}/"
     try:
         with open(t+'a','rb') as f:ta = pickle.load(f)
     except Exception:
-        s4("[雨声]未找到地图册",1)
+        s3("[雨声]未找到地图册")
+        return
     i = Image.open("lab.png")
     l = ImageDraw.Draw(i)
     for z in range(16):
@@ -274,16 +305,16 @@ def f2():
                 c = (ta[x][y][z][0],ta[x][y][z][1],ta[x][y][z][2]) 
                 if c!= (0,0,0):
                     l.rectangle([16*x+2,16*y+288*z+2,16*x+17,16*y+288*z+17],fill=c)
-    s1()
-    print(f"[查看地检查]图册{t}")
+    s0()
+    print(f"[检查]查看地图册\033[94m{t}")
     i.show()
-    s1()
+    s0()
 def f3(l):
     if len(l) == 1:
-        s3("4.1 文件目录",2)
-        s3("4.2 查看预设",2)
-        s3("4.3 查看地图册",2)
-        l = "4." + s4("[检查]选择支线 4.",1)
+        s3("4.1 文件目录")
+        s3("4.2 查看预设")
+        s3("4.3 查看地图册")
+        l = "4." + s7("[检查]选择支线 4.")
     if l == "4.0":
         return
     elif l == "4.1":f0( )
@@ -291,53 +322,84 @@ def f3(l):
     elif l == "4.3":f2( )
 def p0():
     print("[pickle转json]")
-    p = s4("[雨声]指定pickle_",2)
-    j = s4("[雨声]指定json_",2)
+    p = s7("[雨声]指定pickle")
+    j = s7("[雨声]指定json")
     with open(p,'rb') as f:
         d = pickle.load(f)
     try:
         d = json.dumps(d,indent=1)
         with open(j,'w') as f:
             f.write(d)
-        s3(f"[雨声]保存文件_{j}",2)
+        s5(f"[雨声]保存文件",j)
     except Exception as e:print(e)
 def p1():
     print("[json转pickle]")
-    j = s4("[雨声]指定json_",2)
-    p = s4("[雨声]指定pickle_",2)
+    j = s7("[雨声]指定json")
+    p = s7("[雨声]指定pickle")
     with open(j,'r') as f:
         d = json.load(f)
     with open(p,'wb') as f:
         pickle.dump(d,f)
-    s3(f"[雨声]保存文件_{p}",2)
-print("[星光]_1.12.4")
-while True:
-    s2(1)
-    l = s4("[雨声]选择支线_",1)
-    if l == "0":
-        break
-    elif l[0] == "1":g1( )
-    elif l[0] == "2":g3(l)
-    elif l[0] == "3":g4( )
-    elif l[0] == "4":f3(l)
-    elif l[0] == "5":p0( )
-    elif l[0] == "6":p1( )
-    else:
+    s5(f"[雨声]保存文件",p)
+def laer():
+    while True:
         s1()
-        print("""
-[提示]
-1[编辑资源包]
+        l = s7("[雨声]选择支线")
+        if l == "0":
+            break
+        elif l[0] == "1":g1( )
+        elif l[0] == "2":g3(l)
+        elif l[0] == "3":g4( )
+        elif l[0] == "4":f3(l)
+        elif l[0] == "5":p0( )
+        elif l[0] == "6":p1( )
+        else:
+            s0()
+            print("""\033[90m[提示]
+|
+1[编辑预设]
+|_输入预设名
+|   |_选择索引[0~255]
+|   |_输入名称
+|   |_输入主色
+|   |_输入副色
+|
 2[编辑地图册]
+|_输入预设名
+|_输入地图册名
 |   2.1[放置单个方块]
-|   |选择坐标
+|   |_坐标x
+|   |_坐标y
+|   |_坐标z
 |   2.2[放置方块组]
-｜
+||_起点坐标x
+|   |_起点坐标y
+|   |_起点坐标z
+|   |_终点坐标x
+|   |_终点坐标y
+|   |_终点坐标z
 |   2.3[设置笔刷]
+|   |_选择索引[0~255]
+|
 3[生成例图]
+|_输入地图册名
+|_输入文件名
+|
 4[检查目录]
-|   4.1[检查文件目录]
-|   4.2[检查资源包]
+|   4.1[检查文件]
+|   4.2[检查预设]
+|   |_输入预设名
 |   4.3[检查地图册]
-5[导入资源包]
-6[导出资源包]""")
-        s1()
+|   |_输入地图册名
+|
+5[导入文件]
+|_输入数据路径
+|_输入文本路径
+|
+6[导出文件]
+|_输入文本路径
+|_输入数据路径""")
+            s0()
+s0()
+print("[星光]_1.15.1")
+laer()
