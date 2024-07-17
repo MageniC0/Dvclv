4e4d1adbc8ffa191fc4ac5ffcbcdaf8e3450b089
MageniC <168565191+MageniC0@users.noreply.github.com>
2024-07-18 07:59:29 +0800

    Update yushengjun.py

--- yushengjun.py
+++ yushengjun.py
@@ -1,10 +1,31 @@
-def pur():
-    print('_'*199)
-def n1():
-    print('|')
-def n2():
-    print('|   |')
-def sn1(s):
-    print(f'|   {s}')
-def sn2(s):
-    print(f'|   |   {s}')
+class ysj:
+    def __init__(self,theme):
+        self.ind = 1
+        self.ash = '|   '
+        self.dawn = '\033[0m'
+        self.dawn2 = '\033[92m'
+        self.dawn4 = '\033[94m'
+    
+    print(f'[{self.theme}]启动！')
+    
+    def adl(self):
+        self.ind += 1
+
+    def pur(self):
+        print('_' * 199)
+
+    def voidline(self):
+        print(f'{self.dawn2}{self.ash * self.ind}')
+
+    def printline(self, string):
+        print(f'{self.dawn2}{self.ash * self.ind}{self.dawn}{string}')
+
+    def inputline(self, string):
+        inp = input(f'{self.dawn2}{self.ash * self.ind}{self.dawn}{string}{self.dawn4}_')
+        return inp
+
+pr = ysj("test")
+pr.pur()
+pr.voidline()
+pr.adl()
+pr.printline("别炸了！")
