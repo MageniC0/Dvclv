6b7546680af4d31b9cb636bdd9a3a3462e2472ad
MageniC <168565191+MageniC0@users.noreply.github.com>
2024-07-18 13:12:11 +0800

    Update yushengjun.py

--- yushengjun.py
+++ yushengjun.py
@@ -1,18 +1,19 @@
 class ysj:
     def __init__(self,theme):
-        print('_'*199)
-        self.theme = theme
-        print(self.theme)
+        print('_'*39)
+        print(theme)
         self.ind = 1
         self.ash = '|   '
         self.dawn = '\033[0m'
+        self.dawn = '\033[90m'
+        self.dawn1 = '\033[91m'
         self.dawn2 = '\033[92m'
         self.dawn4 = '\033[94m'
     
     def adl(self):
         self.ind += 1
     def pur(self):
-        print('_' * 199)
+        print('_' * 39)
     def voidline(self):
         print(f'{self.dawn2}{self.ash*self.ind}')
     def printline(self,string):
@@ -21,8 +22,9 @@ class ysj:
         return input(f'{self.dawn2}{self.ash*self.ind}{self.dawn}{string}{self.dawn4}_')
     def callline(self,string,object):
         print(f'{self.dawn2}{self.ash*self.ind}{string}{self.dawn4}{object}')
-
+'''
 pr = ysj("test")
 pr.voidline()
 pr.adl()
 pr.printline("别炸了！")
+'''
