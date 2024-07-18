945a2ae44998ad3d401e62a821bddc2aa1e1e6a6
MageniC <168565191+MageniC0@users.noreply.github.com>
2024-07-18 08:36:03 +0800

    Update yushengjun.py

--- yushengjun.py
+++ yushengjun.py
@@ -9,21 +9,12 @@ class ysj:
         self.dawn2 = '\033[92m'
         self.dawn4 = '\033[94m'
     
-    def adl(self):
-        self.ind += 1
-
-    def pur(self):
-        print('_' * 199)
-
-    def voidline(self):
-        print(f'{self.dawn2}{self.ash * self.ind}')
-
-    def printline(self, string):
-        print(f'{self.dawn2}{self.ash * self.ind}{self.dawn}{string}')
-
-    def inputline(self, string):
-        inp = input(f'{self.dawn2}{self.ash * self.ind}{self.dawn}{string}{self.dawn4}_')
-        return inp
+    def adl(self):self.ind += 1
+    def pur(self):print('_' * 199)
+    def voidline(self):print(f'{self.dawn2}{self.ash*self.ind}')
+    def printline(self,string):print(f'{self.dawn2}{self.ash*self.ind}{self.dawn}{string}')
+    def inputline(self,string):return input(f'{self.dawn2}{self.ash*self.ind}{self.dawn}{string}{self.dawn4}_')
+    def callline(self,string,object):print(f'{self.dawn2}{self.ash*self.ind}{string}{dawn4}{object}')
 
 pr = ysj("test")
 pr.voidline()
