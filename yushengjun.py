13656cb3b8c4e52617472b377c0241f96187beb7
MageniC <168565191+MageniC0@users.noreply.github.com>
2024-07-18 13:08:22 +0800

    Update yushengjun.py

--- yushengjun.py
+++ yushengjun.py
@@ -9,12 +9,18 @@ class ysj:
         self.dawn2 = '\033[92m'
         self.dawn4 = '\033[94m'
     
-    def adl(self):self.ind += 1
-    def pur(self):print('_' * 199)
-    def voidline(self):print(f'{self.dawn2}{self.ash*self.ind}')
-    def printline(self,string):print(f'{self.dawn2}{self.ash*self.ind}{self.dawn}{string}')
-    def inputline(self,string):return input(f'{self.dawn2}{self.ash*self.ind}{self.dawn}{string}{self.dawn4}_')
-    def callline(self,string,object):print(f'{self.dawn2}{self.ash*self.ind}{string}{self.dawn4}{object}')
+    def adl(self):
+        self.ind += 1
+    def pur(self):
+        print('_' * 199)
+    def voidline(self):
+        print(f'{self.dawn2}{self.ash*self.ind}')
+    def printline(self,string):
+        print(f'{self.dawn2}{self.ash*self.ind}{self.dawn}{string}')
+    def inputline(self,string):
+        return input(f'{self.dawn2}{self.ash*self.ind}{self.dawn}{string}{self.dawn4}_')
+    def callline(self,string,object):
+        print(f'{self.dawn2}{self.ash*self.ind}{string}{self.dawn4}{object}')
 
 pr = ysj("test")
 pr.voidline()
