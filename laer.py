b8af9389077ad97948644d18248740dcc95ae8b7
MageniC <168565191+MageniC0@users.noreply.github.com>
2024-07-18 20:05:36 +0800

    Update laer.py

--- laer.py
+++ laer.py
@@ -1,7 +1,36 @@
 import os,pickle,json,time
 from PIL import Image,ImageDraw
-from yushengjun import ysj
 #  © 2024 Dustormn.  All rights reserved.
+
+print('_'*39)
+print('[starlit] 1.16.0')
+ind = 1
+        self.ash = '|   '
+        self.dawn = '\033[0m'
+        self.dawn = '\033[90m'
+        self.dawn1 = '\033[91m'
+        self.dawn2 = '\033[92m'
+        self.dawn3 = '\033[93m'
+        self.dawn4 = '\033[94m'
+        self.dawn5 = '\033[95m'
+    
+    def lad(self):
+        self.ind += 1
+    def lsi(self):
+        if self.ind != 0:
+            self.ind -= 1
+        else:
+            raise ValueError("unexpected value 0.")
+    def purline(self):
+        print('_' * 39)
+    def voidline(self):
+        print(f'{self.dawn2}{self.ash*self.ind}')
+    def printline(self,string):
+        print(f'{self.dawn2}{self.ash*self.ind}{self.dawn}{string}')
+    def inputline(self,string):
+        return input(f'{self.dawn2}{self.ash*self.ind}{self.dawn}{string}{self.dawn4}_')
+    def callline(self,string,object):
+        print(f'{self.dawn2}{self.ash*self.ind}{string}{self.dawn4}{object}')
 
 yu = ysj("[stralit] 1.16.0")
 
